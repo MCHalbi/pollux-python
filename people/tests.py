fce00553980a0faec8458b6e0cefc02ac2928042
Lukas Halbritter <halbi93@gmx.de>
2023-05-19 11:31:46 +0200

    Added mixin class for ValidationError checking.

--- people/tests.py
+++ people/tests.py
@@ -3,7 +3,20 @@ from django.test import TestCase
 from datetime import date
 from people.models import Person
 
-class PeopleModelTest(TestCase):
+class TestValidationErrorMixin:
+    def assertValidationError(self, model_object, field_name, message=None):
+        try:
+            model_object.full_clean()
+        except ValidationError as e:
+            self.assertTrue(
+                field_name in e.message_dict,
+                f"The value \"{getattr(model_object, field_name)}\" of "
+                + f"field \"{field_name}\" should be invalid, but it was not.")
+            if message:
+                self.assertEqual(
+                    e.message_dict[field_name][0], message)
+
+class PeopleModelTest(TestCase, TestValidationErrorMixin):
     def test_saving_and_retrieving_people(self):
         person = Person()
         person.title = "Herr"
@@ -25,9 +38,7 @@ class PeopleModelTest(TestCase):
     def test_valitadion_of_title_field(self):
         person = Person(title="FooBar")
 
-        try:
-            person.full_clean()
-        except ValidationError as e:
-            self.assertTrue(
-                "title" in e.message_dict,
-                "The value of \"title\" should be invalid, but it was not.")
+        self.assertValidationError(
+            person,
+            "title",
+            "Value 'FooBar' is not a valid choice.")
